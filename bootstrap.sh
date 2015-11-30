b402d26542476a90152ab7e50a7a2c5d9bbdb014
dmccuk <dmccuk@gmail.com>
2015-11-30 14:40:04 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,21 +8,22 @@ IP=`ip a | grep "scope global eth1" | awk '{print $2}' | awk -F/ '{print $1}'`
 
 #read HOSTNAME?"Enter hostname: "
 
-HOSTNAME=puppet-client.den.com
+HOSTNAME=puppet-client.den.com #make this a variable
 echo $HOSTNAME "will be used. finishing the build..."
 sed -i "s/HOSTNAME=.*/HOSTNAME=$HOSTNAME/g" /etc/sysconfig/network
 hostname "$HOSTNAME"
 
 cat >> /etc/hosts <<EOF
 192.168.0.18 puppet puppet-master.den.com
+192.168.0.17 http-repo.den.com
 $IP	$HOSTNAME $HOSTNAME.den.com
 EOF
 
-cat >> /etc/resolv.conf <<EOF
+cat > /etc/resolv.conf <<EOF
 search den.com
 nameserver 192.168.0.1
 EOF
-chattr +i /etc/resolv.conf
+chattr +a /etc/resolv.conf
 
 cat > /etc/motd <<EOF
 Welcome to $hostname
@@ -33,18 +34,17 @@ EOF
 cat > /etc/yum.repos.d/Puppet-Repo.repo <<EOF
 [puppetrepo]
 name=Puppet Repo
-baseurl=http://192.168.0.17/puppet
+baseurl=http://http-repo.den.com/puppet
 gpgcheck=0
 enabled=1
 EOF
 
 yum clean all
 yum -y install puppet
-sleep 2
-puppet agent -t
 sleep 5
 puppet config set environment development --section agent
 puppet config set server puppet-master.den.com --section agent
 puppet agent -t
-sleep 2
+sleep 5
+puppet agent -t
 reboot
